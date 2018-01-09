b87a9a5386f6afdcc8fccf42496a582a88889ea9
kochetkov_ma <maksim.kochetkov@open.ru>
2018-01-09 09:45:06 +0300

    java docs desc

--- web/src/main/java/ru/mk/pump/web/elements/api/concrete/complex/Child.java
+++ web/src/main/java/ru/mk/pump/web/elements/api/concrete/complex/Child.java
@@ -30,17 +30,31 @@ public class Child<T extends Element> {
         this.parentElement = parentElement;
     }
 
+    /**
+     * Set default locator. If parent element has special locator it will be rewrite
+     */
     public Child<T> withDefaultBy(@Nullable By[] defaultByArray) {
         this.defaultByArray = defaultByArray;
         return this;
     }
 
+    /**
+     * Find child element and write to local cache
+     * @param childClass Child class element to create if any element has been found
+     * @return new child element
+     */
     public T find(Class<T> childClass) {
         cache = parentElement.getSubElements(childClass).find(extractBys());
-        ((BaseElement)cache).withParams(parentElement.getParams());
+        ((BaseElement) cache).withParams(parentElement.getParams());
         return cache;
     }
 
+    /**
+     * If cache is not null return child element from cache. Else call {@link #find(Class)}
+     *
+     * @param childClass Child class element to create if any element has been found
+     * @return new child element
+     */
     public T get(Class<T> childClass) {
         if (cache == null) {
             return find(childClass);
@@ -48,6 +62,10 @@ public class Child<T extends Element> {
         return cache;
     }
 
+    /**
+     * If child element is optional. Check is child element has any locators for search.
+     * Use this check before any actions with child element
+     */
     public boolean isDefined() {
         return ArrayUtils.isNotEmpty(extractBys());
     }
